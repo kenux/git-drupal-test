89571750868202e8bb2d1238ad5d8f603b7b9eab
Kenneth <kenn.net@gmail.com>
2023-09-25 22:21:48 -0400

    Update vuln.php

--- vuln.php
+++ vuln.php
@@ -1,4 +1,15 @@
-<?php 
+<?php
 
-$command = "mv ".$_SERVER['HTTP_X_FILE']." uploads/";
-$output = system($command);
+/**
+ * @return array<string>
+ */
+function takesAnInt(int $i) {
+    return [$i, "hello"];
+}
+
+$data = ["some text", 5];
+takesAnInt($data[0]);
+
+$condition = rand(0, 5);
+if ($condition) {
+} elseif ($condition) {}
